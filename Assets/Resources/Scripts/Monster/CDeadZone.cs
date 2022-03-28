d23955f077d4a3c6aa5875c50c934edd2ca8f1bd
yunon <38744862+yunonXD@users.noreply.github.com>
2022-03-29 00:16:18 +0900

    보스 업데이트_4
    - KillZone 접근시 플레이어 처형(즉사) 처리
    - 리소스 변경(Run 애니메이션)
    - 소소한 스크립트 수정
    - 더미보스 크기 수정

--- Assets/Resources/Scripts/Monster/CDeadZone.cs
+++ Assets/Resources/Scripts/Monster/CDeadZone.cs
@@ -2,31 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CDeadZone : MonoBehaviour
+public class CDeadZone : CControllerBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void init(CDynamicObject actor)
     {
-        
+        gameObject.SetActive(true);
+        base.init(actor);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider col)
+    //When player touches, WayPoint script will change Boss speed to 0, play execution motion and delete player.
+    //플레이어 닿으면 WayPoint 스크립트에서 boss 스피드 0 으로 바꾸고 처형 모션 재생 및 플레이어 삭제
     {
-        if (col.CompareTag("Player")){
-
-            Destroy(GameObject.FindGameObjectWithTag("Player"));
-
-        }
-        else
+        if (col.CompareTag("Player"))
         {
-            return;
+            CWayPoint cWayPoint = GameObject.Find("WayPointFollower").GetComponent<CWayPoint>();
+            cWayPoint.g_speed = 0;
+            m_Actor.g_Animator.SetTrigger("AttackReady01");
+
+            StartCoroutine(AttackDelay());
         }
+
+    }
+
+    IEnumerator AttackDelay()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
 
     }
 

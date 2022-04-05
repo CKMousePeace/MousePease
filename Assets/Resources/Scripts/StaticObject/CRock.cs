34e1018030da59f994a492b1f5b50207c1645d43
yunon <38744862+yunonXD@users.noreply.github.com>
2022-04-06 02:38:28 +0900

    보스업데이트V10p1
    범위 안에 일정시간동안 서 있으면 데미지 입는 함정 추가
    (제1 공격패턴에 근거함)
    But. 아직 테스트 버전으로
    
    링이 있고 그 곳이 채워지는 방식이 아니라
    링 자체가 축소되는 방법이고
    플레이어가 링이 완전히 축소할때 까지 (유저가 지정한 크기가 될 때 까지)
    해당 지점에 콜라이더가 닿아있으면 버프 호출 , 그리고 해당 함정 비활성화.

--- Assets/Resources/Scripts/StaticObject/CRock.cs
+++ Assets/Resources/Scripts/StaticObject/CRock.cs
@@ -6,25 +6,26 @@ using System;
 public class CRock : CStaticObject
 {
 
-    [SerializeField]  private Vector3 startPos, endPos;
+   //[SerializeField]  private Vector3 startPos, endPos;
    //땅에 닫기까지 걸리는 시간
 
-    [SerializeField] protected float timer;
-    [SerializeField] protected float Size;
-
-    float testboii;
+    //줄어드는 시간
+    [SerializeField] protected float m_timer;
 
 
+    //[SerializeField] protected float Size;
 
     [Tooltip("Float[0] = Force, Float[1] = Damage")]
     [SerializeField] private CBuffBase.BuffInfo m_Buffinfo;
 
 
+    private bool isCheck = false;
 
     protected override void Start()
     {
         base.Start();
         m_Buffinfo.g_Value_Vector3.Add(transform.position);
+
 
     }
 
@@ -32,76 +33,73 @@ public class CRock : CStaticObject
     {
 
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            StartCoroutine("Start");
-        }
     }
-
-    IEnumerator EStart()
-    {
-        startPos = transform.position;
-        endPos = startPos + new Vector3(5, 0, 0);
-        StartCoroutine("BulletMove");
-
-        yield return new WaitForEndOfFrame();
-    }
-
 
 
 
 
     private void OnTriggerEnter(Collider col)
     {
-        var actor = col.gameObject.GetComponent<CDynamicObject>();
-        if (actor == null) return;
-        
-
         if (col.CompareTag("Player"))
         {
+            //변경할 크기 값      size value to change
+            var scaleTo = new Vector3(0.0f, 0.0f, 0.1f);
 
-            StartCoroutine("RockAttackReady");
-            actor.GenerateBuff("KnockBack", m_Buffinfo);
+            //게임 오브젝트 , 변경할 크기 값, 소요시간  GameObject , size value to change, time
+            StartCoroutine(DangerZoneChecker(gameObject, scaleTo, m_timer));
+
+
         }
 
     }
-    IEnumerator RockAttackReady()
-    {
 
-        transform.localScale = Vector3.one * (1 + testboii);
-        testboii += Time.deltaTime;
-        if(testboii > 1f)
+
+    IEnumerator DangerZoneChecker(GameObject objectToScale, Vector3 scaleTo, float seconds)
+    {
+        var actor = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<CDynamicObject>();
+
+        float elapsedTime = 0;
+        Vector3 startingScale = objectToScale.transform.localScale;
+        while (elapsedTime < seconds)
         {
-            gameObject.SetActive(false);
-        }
-
-
-        yield return new WaitForSeconds(timer);
-
-    }
-
-
-
-    protected static Vector3 RockFalling(Vector3 start, Vector3 end, float height, float t)
-    {
-        Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
-
-        var mid = Vector3.Lerp(start, end, t);
-
-        return new Vector3(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t), mid.z);
-        //x의 이동(직선) , y 이동을 이차곡선 형태로 형성     x movement (straight line) , y movement is formed in the form of a quadratic curve
-    }
-
-    protected IEnumerator BulletMove()
-    {
-        timer = 0;
-        while (transform.position.y >= startPos.y)
-        {
-            timer += Time.deltaTime;
-            Vector3 tempPos = RockFalling(startPos, endPos, 5, timer);
-            transform.position = tempPos;
+            objectToScale.transform.localScale = Vector3.Lerp(startingScale, scaleTo, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+        objectToScale.transform.position = scaleTo;
+        isCheck = true;
+
+
+        if (isCheck == true)
+        {
+            actor.GenerateBuff("KnockBack", m_Buffinfo);
+            gameObject.SetActive(false);
+        }
+        
     }
+
+
+
+    //protected static Vector3 RockFalling(Vector3 start, Vector3 end, float height, float t)
+    //{
+    //    Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+    //    var mid = Vector3.Lerp(start, end, t);
+
+    //    return new Vector3(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t), mid.z);
+    //    //x의 이동(직선) , y 이동을 이차곡선 형태로 형성     x movement (straight line) , y movement is formed in the form of a quadratic curve
+    //}
+
+    //protected IEnumerator BulletMove()
+    //{
+    //    timer = 0;
+    //    while (transform.position.y >= startPos.y)
+    //    {
+    //        timer += Time.deltaTime;
+    //        Vector3 tempPos = RockFalling(startPos, endPos, 5, timer);
+    //        transform.position = tempPos;
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
 
 }

81f5cf9480583a7029c3e805352442a325ba1e48
choiHyeongsun <chlgudtjn3@gmail.com>
2022-04-16 22:13:21 +0900

    머지 충돌 해결

--- Assets/Resources/Scripts/StaticObject/ArtilleryZone/CCheese.cs
+++ Assets/Resources/Scripts/StaticObject/ArtilleryZone/CCheese.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CCheese : MonoBehaviour
+public class CCheeseTest : MonoBehaviour
 {
     private bool isChecker = false;
 

6ea98f7c98809188e01bf39d87f5280b73ac1f92
choihyeongsun <cholgudtjn3@gmail.com>
2022-05-27 20:23:40 +0900

    플레이어 기믹 추가

--- Assets/Resources/Scripts/ScriptBase/CBuffBase.cs
+++ Assets/Resources/Scripts/ScriptBase/CBuffBase.cs
@@ -12,9 +12,9 @@ public abstract class CBuffBase : MonoBehaviour
         [SerializeField] private List<float> m_Value_Float;
         [SerializeField] private List<Vector3> m_Value_Vector3;
 
-        public List<int> g_Value_Int => m_Value_Int;
-        public List<float> g_Value_Float => m_Value_Float;
-        public List<Vector3> g_Value_Vector3 => m_Value_Vector3;
+        public List<int> g_Value_Int { get { return m_Value_Int; } set { m_Value_Int = value; } }
+        public List<float> g_Value_Float { get { return m_Value_Float; } set { m_Value_Float = value; } }
+        public List<Vector3> g_Value_Vector3 { get { return m_Value_Vector3; } set { m_Value_Vector3 = value; } }
     }
 
 

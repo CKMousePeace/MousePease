20192b7c072b9c73738854909da56d0b60109320
choihyeongsun <cholgudtjn3@gmail.com>
2022-05-29 01:54:39 +0900

    점프 패드 버그 수정

--- Assets/Resources/Scripts/OtherScript/CJumpPad.cs
+++ Assets/Resources/Scripts/OtherScript/CJumpPad.cs
@@ -6,15 +6,29 @@ public class CJumpPad : CStaticObject
 {
 
     [SerializeField] private float m_Force;
-
+    private CPlayer m_Player;
+    private Rigidbody rb;
 
     private void OnCollisionEnter(Collision collision)
     {        
         if (collision.transform.CompareTag("Player"))
-        {            
-            Rigidbody rb = collision.transform.GetComponent<Rigidbody>();
+        {   
+            if (rb == null)
+                rb = collision.transform.GetComponent<Rigidbody>();
+
+            if (m_Player == null)
+                m_Player = collision.transform.GetComponent<CPlayer>();
+
+
+            if (m_Player.CompareController("Jump"))
+            {
+                m_Player.DestroyController("Jump");
+            }
+
             rb.velocity = Vector3.zero;
             rb.velocity = Vector3.up * m_Force;
+
+
         }
     }
     

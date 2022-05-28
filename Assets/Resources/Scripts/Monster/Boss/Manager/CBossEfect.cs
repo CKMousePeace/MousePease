e82e77b9f1fd390b28d7ce8f12b8e5186e5d8253
yunon <38744862+yunonXD@users.noreply.github.com>
2022-05-28 22:37:21 +0900

    보스업데이트V21p9
    애니메이션클립 추가 (스크립트)_

--- Assets/Resources/Scripts/Monster/Boss/Manager/CBossEfect.cs
+++ Assets/Resources/Scripts/Monster/Boss/Manager/CBossEfect.cs
@@ -7,6 +7,18 @@ public class CBossEfect : MonoBehaviour
     [SerializeField] ParticleSystem Footsteb_L;
     [SerializeField] ParticleSystem Footsteb_R;
 
+    [SerializeField] ParticleSystem Idle_buble;
+    [SerializeField] ParticleSystem Idle_MouthSmell;
+    [SerializeField] ParticleSystem Idle_Start_L;
+    [SerializeField] ParticleSystem Idle_Start_R;
+    [SerializeField] ParticleSystem Idle_Saliva;
+    [SerializeField] ParticleSystem Idle_Aura;
+
+    [SerializeField] ParticleSystem Attack_Roar;
+    [SerializeField] ParticleSystem Attack_Bite;
+
+    [SerializeField] ParticleSystem Attack_HoldDown;
+
     [SerializeField] GameObject FL_;
     [SerializeField] GameObject FR_;
         
@@ -35,6 +47,21 @@ public class CBossEfect : MonoBehaviour
         Footsteb_R.transform.position = FR;
         Footsteb_R.Play();
         
+    }
+
+    public void PlayRoar()
+    {
+        Attack_Roar.Play();
+    }
+
+    public void PlayBite()
+    {
+        Attack_Bite.Play();
+    }
+
+    public void PlayHoldDown()
+    {
+        Attack_HoldDown.Play();
     }
 
 }

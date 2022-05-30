8f3352da67433b878d46a12be051d8ed8fd97242
yunon <38744862+yunonXD@users.noreply.github.com>
2022-05-31 02:40:51 +0900

    보스업데이트V22p6 플레이어 사운드 추가

--- Assets/Resources/Scripts/Item/CDashItem.cs
+++ Assets/Resources/Scripts/Item/CDashItem.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class CDashItem : CItemBase
 {
     [SerializeField] private float m_RotateSpeed = 100f;
+    private FMOD.Studio.EventInstance SFX_DashItem;
 
     private void FixedUpdate()
     {
@@ -21,5 +23,17 @@ public class CDashItem : CItemBase
         if (Dash == null) return;
 
         Dash.g_DashItem = true;
+
+        PlayDashItem();
+
+
     }
+    private void PlayDashItem()                 //플레이어 아이템 습득하는 소리
+    {
+        SFX_DashItem = RuntimeManager.CreateInstance("event:/Interactables/SFX_DashItem");
+        SFX_DashItem.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+        SFX_DashItem.start();
+        SFX_DashItem.release();
+    }
+
 }

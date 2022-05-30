f9b4e6f192ba5c559c2cb7f72fe61f22562c61af
yunon <yunonXD@users.noreply.github.com>
2022-05-30 16:57:18 +0900

    보스업데이트V22p1 간단한 코드 정리

--- Assets/Resources/Scripts/OtherScript/===Test Progress ===/CBoomBoomCheese.cs
+++ Assets/Resources/Scripts/OtherScript/===Test Progress ===/CBoomBoomCheese.cs
@@ -6,24 +6,32 @@ public class CBoomBoomCheese : MonoBehaviour
     [SerializeField] private GameObject m_BoomCheese;
     [SerializeField] private float m_BoomForce;
 
-    //ÀÌ°Å ¾È¾¸ °ÆÁ¤¤¤¤¤
-    //´Ù½Ã °ÆÁ¤ ¤·¤· ¾µµí
+    [Header("ÆÄ±« µô·¹ÀÌ ÃÊ´ÜÀ§")]
+    [SerializeField] private int DelayTime = 1;
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter(Collision col)
     {
-        if (collision.gameObject.CompareTag("Boss"))
+        if (col.gameObject.CompareTag("Boss"))
         {
-            GameObject BCS = Instantiate(m_BoomCheese, transform.position, transform.rotation);
-
-            foreach (Rigidbody rb in BCS.GetComponentsInChildren<Rigidbody>())
-            {
-                Vector3 force = (rb.transform.position - transform.position).normalized * m_BoomForce;
-                rb.AddForce(force);
-            }
-
-            Destroy(gameObject);
+            StartCoroutine(DelayBoomBoom(DelayTime));
         }
         else return;
+    }
+
+    IEnumerator DelayBoomBoom(int delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        GameObject BCS = Instantiate(m_BoomCheese, transform.position, transform.rotation);
+
+        foreach (Rigidbody rb in BCS.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * m_BoomForce;
+            rb.AddForce(force);
+        }
+
+        Destroy(gameObject);
     }
 
 }

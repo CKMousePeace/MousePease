002bb156b65175807eb47d660c0c7c4a94af4c6b
yunon <38744862+yunonXD@users.noreply.github.com>
2022-06-02 23:40:15 +0900

    보스업데이트V25 굴러 떨어지는 통나무 더미 Rolling Object 추가. 두리안? 듀라안? 과 매우 비슷하게 제작되어있음

---
+++ Assets/Resources/Scripts/StaticObject/RollinSpawn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollinSpawn : MonoBehaviour
+{ 
+    [SerializeField] private Transform m_RollingSpawnLocation;
+    [SerializeField] private float m_WaitTime = 0;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StartCoroutine(SpawnRolling());
+        }
+    }
+
+
+    IEnumerator SpawnRolling()
+    {
+
+        yield return new WaitForSeconds(m_WaitTime);
+
+       CObjectPool.g_instance.ObjectPop("RollingBoi", new Vector3(m_RollingSpawnLocation.position.x,
+                        m_RollingSpawnLocation.position.y, m_RollingSpawnLocation.position.z), Quaternion.Euler(0, 0, 0), new Vector3(1.0f, 1.0f, 1.0f));
+       CObjectPool.g_instance.ObjectPush();
+
+        Destroy(gameObject);
+    }
+
+}
